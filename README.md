6e8742f9a609c6f048720f970ad65021f7643a08
Shaun Li <shonhen@gmail.com>
2022-09-16 14:42:27 +0800

    Another vue-router inspired Svelte router

---
+++ README.md
@@ -0,0 +1,7 @@
+# Svelte Router
+
+Another vue-router inspired Svelte router
+
+## License
+
+Licensed under [MIT](./LICENSE)
---
+++ lib/Link.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from 'svelte'
+  import { config, updateState } from './route'
+  import { activity } from './actions'
+
+  export let href
+  export let replace = false
+  export let activeClass = 'active'
+  export let exactActiveClass = 'exact-active'
+
+  onMount(() => {
+    if (config.mode === 'hash') href = '#' + href
+    if (!href.startsWith(config.base)) href = config.base + href
+  })
+
+  function go() {
+    let path = location.href.replace(location.origin, '')
+    if (config.mode === 'hash') path = path.substring(location.pathname.length)
+
+    if (path != href) {
+      replace
+        ? history.replaceState({}, '', href)
+        : history.pushState({}, '', href)
+
+      updateState()
+    }
+  }
+</script>
+
+<a {href} on:click|preventDefault={go} {...$$restProps} use:activity={{activeClass, exactActiveClass}}><slot></slot></a>
